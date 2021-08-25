1708afc6820a8dd46244e33a9af55346db4baa21
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-25 15:11:12 -0700

    Added the top bar and did some changes in the state using useState and added a background image

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 
 import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
 import React from 'react'
+import { useState } from 'react'
 import './navbar.scss'
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] =  useState(false)
+    
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+    }
     return (
-        <div className="navbar">
+        <div className={isScrolled ? "navbar scrolled": "navbar"}>
             <div className="container">
                 <div className="left">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" srcset="" />
