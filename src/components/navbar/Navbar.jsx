e362cb50bd18da222c8767b5b6284fac67c603a9
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-25 11:41:19 -0700

    i added the navigation bar and did some stylings usins scss

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
+import React from 'react'
+import './navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" srcset="" />
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+                <div className="right">
+                    <Search className="icon"/>
+                    <span>KID</span>
+                    <Notifications className="icon"/>
+                    <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+                    <div className="profile">
+                    <ArrowDropDown className="icon"/>
+                    <div className="options">
+                        <span>Settings</span>
+                        <span>Logout</span>
+                    </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,13 +1,55 @@
-import { AcUnit } from '@material-ui/icons'
 import React from 'react'
+import Navbar from '../components/navbar/Navbar'
 import './home.scss'
 
 
 const Home = () => {
     return (
         <div className="home">
-            <h3>Hello</h3>
-            <AcUnit/>
+            <Navbar/>
+            <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita non sequi repudiandae minima minus nulla dolores iste porro molestiae, explicabo quisquam fugit tenetur alias hic sint beatae perferendis accusantium error.
         </div>
     )
 }
