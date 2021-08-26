bc4cd0b733d605401448680ffba5ed4d6a741401
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-26 09:26:48 -0700

    added the images in the list items

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -27,23 +27,16 @@ export default function List() {
             <div className="wrapper">
                 <ArrowBackIosOutlined className="sliderArrow left" onClick= { () =>  handleClick("left")} style={{display: !isMoved && 'none'}}  />
                 <div className="container" ref={listRef}>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
+                    <ListItem index={0}/>
+                    <ListItem index={1}/>
+                    <ListItem index={2}/>
+                    <ListItem index={3}/>
+                    <ListItem index={4}/>
+                    <ListItem index={5}/>
+                    <ListItem index={6}/>
+                    <ListItem index={7}/>
+                    <ListItem index={8}/>
+                    <ListItem index={9}/>
                 </div>
                 <ArrowForwardIosOutlined className="sliderArrow right" onClick= { () =>  handleClick("right")}/>
             </div>
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from '@material-ui/icons';
+import React, { useState } from 'react'
 import './listitem.scss';
 
-export default function ListItem() {
+export default function ListItem({index}) {
+    const [isHovered, setIsHovered] = useState(false)
     return (
-        <div className="listItem">
-            List item
+        <div className="listItem" style={{left: isHovered && index * 225 - 50}} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+                   alt="" />
+                   <div className="itemInfo">
+                       <div className="icons">
+                           <PlayArrow/>
+                           <Add/>
+                           <ThumbUpAltOutlined/>
+                           <ThumbDownAltOutlined/>
+                       </div>
+                       <div className="itemInfoTop">
+                           <span>1 hour 14 mins</span>
+                           <span className="limit">+16</span>
+                           <span>1999</span>
+                       </div>
+                       <div className="desc">
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nulla recusandae illum 
+                           corrupti repellat laborum dolor culpa porro architecto officia maxime possimus vitae eos
+                       </div>
+                       <div className="genre">Action</div>
+                   </div>
+            
         </div>
     )
 }
