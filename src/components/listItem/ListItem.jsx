5f2e5fb619abda57b151da2f33d2cc2e16cc0511
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-26 11:56:30 -0700

    Added the features and styles in the list items

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -3,17 +3,22 @@ import React, { useState } from 'react'
 import './listitem.scss';
 
 export default function ListItem({index}) {
-    const [isHovered, setIsHovered] = useState(false)
+    const [isHovered, setIsHovered] = useState(false);
+    const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
     return (
-        <div className="listItem" style={{left: isHovered && index * 225 - 50}} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <div className="listItem" style={{left: isHovered && index * 225 - 50 + index * 2.5}} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
                    alt="" />
+                   {isHovered && (
+                       <>
+                   <video src={trailer} autoPlay={true} loop/>
                    <div className="itemInfo">
                        <div className="icons">
-                           <PlayArrow/>
-                           <Add/>
-                           <ThumbUpAltOutlined/>
-                           <ThumbDownAltOutlined/>
+                           <PlayArrow className="icon"/>
+                           <Add className="icon"/>
+                           <ThumbUpAltOutlined className="icon"/>
+                           <ThumbDownAltOutlined className="icon"/>
                        </div>
                        <div className="itemInfoTop">
                            <span>1 hour 14 mins</span>
@@ -22,11 +27,11 @@ export default function ListItem({index}) {
                        </div>
                        <div className="desc">
                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nulla recusandae illum 
-                           corrupti repellat laborum dolor culpa porro architecto officia maxime possimus vitae eos
                        </div>
                        <div className="genre">Action</div>
                    </div>
-            
+                   </>
+                   )}
         </div>
     )
 }
