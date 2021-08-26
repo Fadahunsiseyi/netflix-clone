d34d4cfee5caf2567bfdce3baa9d11b7e8b9b93a
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-26 07:13:37 -0700

    added the move slide numbers using useStates

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,15 +1,32 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons'
 import React from 'react'
+import { useRef, useState } from 'react'
 import ListItem from '../listItem/ListItem'
 import './list.scss'
 
 export default function List() {
+    const [sliderNumber, setSlideNumber] = useState(0)
+    const [isMoved, setIsMoved] = useState(false)
+    const listRef = useRef()
+    const handleClick = (direction) => {
+        setIsMoved(true)
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+        if(direction === 'left' && sliderNumber > 0) {
+            setSlideNumber(sliderNumber - 1);
+            listRef.current.style.transform = `translateX(${230 + distance}px)`
+        }
+        if(direction === 'right' && sliderNumber < 5) {
+            setSlideNumber(sliderNumber + 1);
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`
+        }
+        console.log(distance)
+    }
     return (
         <div className="list">
             <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
-                <ArrowBackIosOutlined className="sliderArrow left"/>
-                <div className="container">
+                <ArrowBackIosOutlined className="sliderArrow left" onClick= { () =>  handleClick("left")} style={{display: !isMoved && 'none'}}  />
+                <div className="container" ref={listRef}>
                     <ListItem/>
                     <ListItem/>
                     <ListItem/>
@@ -28,7 +45,7 @@ export default function List() {
                     <ListItem/>
                     <ListItem/>
                 </div>
-                <ArrowForwardIosOutlined className="sliderArrow right"/>
+                <ArrowForwardIosOutlined className="sliderArrow right" onClick= { () =>  handleClick("right")}/>
             </div>
         </div>
     )
