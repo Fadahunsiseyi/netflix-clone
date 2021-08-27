44db617e423bc3e17994c98d0aff168d55523ce7
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-27 09:07:49 -0700

    Added the list,movie and user models

--- api/models/User.js
+++ api/models/User.js
@@ -1,2 +1,12 @@
 
 const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: {type: string, required: true, unique: true},
+    email: {type: string, required: true, unique: true},
+    password: {type: string, required: true},
+    profilePic: {type: string, default: ""},
+    isAdmin: {type: boolean, default: false}
+},{timestamps: true})
+
+module.export = mongoose.model('User', UserSchema)
