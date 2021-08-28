9f0983689d7a61b3729c7bae50d2535a5f7dcc7f
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-27 22:01:02 -0700

    i created the login route with authentication and stored each user i created in mongoDB

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -10,14 +10,29 @@ router.post('/register', async(req, res) => {
      const newUser = new User({
          username: req.body.username,
          email: req.body.email,
-         password: req.body.password,
+         password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
      })
+     console.log(newUser)
      try {
         const user = await newUser.save()
         res.status(201).json(user)
      } catch(err) {
          res.status(500,'One error occurred').json(err)
      }
-    
 })
+
+//LOGIN
+router.post('/login',(req, res) => {
+      try{
+          const user = User.findOne({email: req.body.email});
+          (!user && res.status(401).json('Wrong password or username'))
+      }
+      catch (err) {
+          res.status(500).json(err);
+      }
+})
+
+
+
+
 module.exports = router;
