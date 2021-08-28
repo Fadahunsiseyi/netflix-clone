908537b0bad6adbce6d33609bb09845a3d849f5e
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-28 15:05:38 -0700

    i added the stats request and ran my test with postman API

--- api/routes/users.js
+++ api/routes/users.js
@@ -79,4 +79,45 @@ router.get('',verify, async (req, res) => {
 
 //GET USER STATS
 
+router.get("/stats", async (req, res) => {
+    const today = new Date()
+    const lastYear = today.setFullYear(today.setFullYear() - 1)
+
+    const monthsArray = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+    try{
+        const data = await User.aggregate([
+            {
+                $project: {
+                    month: {$month: "$createdAt"}
+                }
+            },{
+                $group: { 
+                    _id: "$month",
+                    total: {$sum: 1}
+                }
+            }
+        ])
+        res.status(200).json(data)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+
 module.exports = router
