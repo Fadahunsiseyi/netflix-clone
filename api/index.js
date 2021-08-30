546135b02fd580ce9ff0cec9dfb64d6be40153dc
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-08-30 15:38:46 +0100

    I added the /movie path in the http request

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
+const movieRoute = require("./routes/movies")
 
 dotenv.config()
 
@@ -19,8 +20,9 @@ async function main() {
 
   app.use('/api/auth', authRoute)
   app.use('/api/users', userRoute)
+  app.use('/api/movies', movieRoute)
 
 
 app.listen(8800, ()=> {
     console.log("Back-end server is running")
-})
+}) 
