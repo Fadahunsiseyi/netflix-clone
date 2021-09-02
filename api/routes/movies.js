ffa6fbf2b361ffc10d9371e026745a0ec233c93f
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-09-02 15:28:21 +0100

    added the random endpoint to the get request

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -61,7 +61,7 @@ router.delete('/:id',verify, async (req, res) => {
 
 
 //GET
-router.get('/:id',verify, async (req, res) => {
+router.get('/find/:id',verify, async (req, res) => {
         try {
              const movie =  await Movie.findById(req.params.id)
             res.status(200).json(movie);
